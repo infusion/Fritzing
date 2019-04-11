5b893991eecb357e5e16e953d09ecdfbf8bb76d2
Robert Eisele <robert@xarg.org>
2019-04-11 14:08:59 +0200

    Update demo.ino

--- Nunchuk/demo.ino
+++ Nunchuk/demo.ino
@@ -2,9 +2,12 @@
 #include "Nunchuk.h"
 
 void setup() {
-
     Serial.begin(9600);
     Wire.begin();
+
+     // Change TWI speed for nuchuk, which uses Fast-TWI (400kHz)
+    Wire.setClock(400000);
+
     // nunchuk_init_power(); // A1 and A2 is power supply
     nunchuk_init();
 }
